705096013b54570c8dd550ef3baec7060fc1d297
imfulee <imfulee@gmail.com>
2022-06-20 09:11:46 +0800

    change some formatting for printer

--- API/order/set_order.php
+++ API/order/set_order.php
@@ -7,6 +7,7 @@ use Mike42\Escpos\Printer;
 // initialize printer with font size options
 $connector = new NetworkPrintConnector("192.168.11.100", 9100);
 $printer = new Printer($connector);
+// $printer->setTextSize(2,2);
 /**
  * Get the current order log 
  */
@@ -25,7 +26,7 @@ $table_name = '';
 $table_name_query = mysqli_query($db_link, "SELECT `A01N02CV0255` FROM `a01` WHERE `A01I01XA` = '$table_uuid'");
 $table_name_result = mysqli_fetch_row($table_name_query)[0];
 $printer->textChinese("桌號$table_name_result\n");
-$printer->feed(3);
+$printer->feed(2);
 
 
 // find if b01 has any available records to place under, otherwise use a new UUID
@@ -60,12 +61,15 @@ foreach ($order_list as $order_item) {
             $item_remarks = $item_remarks . ',';
         }
     }
-    $printer->textChinese("$item_name x $item_quantity\n");
-    $printer->textChinese("$item_remarks\n");
-    $printer->feed(1);
+    $printer->textChinese("$item_name x $item_quantity = $ $item_price\n");
+    if ($item_remarks !== ''){
+        $printer->textChinese("$item_remarks\n");
+    }
     $result = $result && mysqli_query($db_link, "INSERT INTO `b02`(`B02I01XA`, `B02I02XA`, `B02N03CV0255`, `B02N04MM`, `B02N05CV0255`, `B02N06CV0255`) VALUES ('$item_uuid','$uuid','$item_name','$item_price','$item_remarks', '$item_quantity')");
 }
 
+$printer->textChinese("------------------------------------------------\n");
+$printer->textChinese("總價：$ $payment_total");
 $printer->feed(3);
 $printer->cut();
 $printer->close();
